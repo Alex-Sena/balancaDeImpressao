a08a7e5c2d223d79b201926352b0eaf724e80ae2
Alex Sandro <asssousa2@gmail.com>
2022-06-16 20:35:23 -0300

    Materiais pesados.

---
+++ balancaDeImpressao.js
@@ -0,0 +1,16 @@
+let peso =3000;
+
+if(peso <2500)
+{
+    console.log("Sair da balança")
+}
+else if(peso >3000)
+    {
+        console.log("Imprimir etiqueta preta")
+    }    
+else
+{
+    console.log("Imprimir etiqueta branca")
+}
+
+
